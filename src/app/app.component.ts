9e8cdf4e347180e34eca79730fec9d42ac48cdb2
Denis Korniychuk <new79den@gmail.com>
2017-01-14 22:32:25 +0300

    Lesson 43 *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
   private switch: boolean = false;
+  private items: number[] = [1,2,3,4,5];
+
   onSwitch(){
     this.switch = !this.switch;
   }
